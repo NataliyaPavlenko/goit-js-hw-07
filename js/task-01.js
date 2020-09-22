375aba9772fb031f100297e5ea1bb918dc3d5aba
Nataliya Pavlenko <mrsnataliya@gmail.com>
2020-09-22 23:44:57 +0300

    add task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,6 @@
 /* В HTML есть список категорий ul#categories.
 Напиши скрипт, который выполнит следующие операции.
+
 Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
 Получится 'В списке 3 категории.'.
 Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента(тега h2) 
@@ -7,3 +8,13 @@
 Например для первой категории получится:
 Категория: Животные
 Количество элементов: 4 */
+
+const categoryList = document.querySelectorAll(".item");
+console.log(`В списке ${categoryList.length} категории.`);
+
+const titleAndNumber = categoryList.forEach((element) =>
+  console.log(
+    `Категория: ${element.querySelector("h2").textContent}
+    Количество элементов: ${element.querySelectorAll("li").length}`
+  )
+);
