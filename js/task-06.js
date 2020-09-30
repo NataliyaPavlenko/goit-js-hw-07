f352a457598fa9ce757057e758d67a421e59ada1
Nataliya Pavlenko <mrsnataliya@gmail.com>
2020-09-30 13:49:20 +0300

    fixed 6 task

--- js/task-06.js
+++ js/task-06.js
@@ -4,7 +4,7 @@
 Если введено подходящее количество, то border инпута становится зеленым, если неправильное - красным.
 Для добавления стилей, используй CSS-классы valid и invalid. */
 
-const input = document.querySelector("#validation-input");
+const validationInputRef = document.querySelector("#validation-input");
 
 function validationInput() {
   if (validationInputRef.value.length === +validationInputRef.dataset.length) {
